de29bd635bb74c98269deb75e2e60c5a2852c598
h576069 <AnneDrottning@annes-macbook-pro-147.home>
2021-04-12 12:05:34 +0200

    Fikset en liten skrivefeil sånn at test ble grønn - Anne

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
@@ -12,56 +12,58 @@ import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Hash { 
-	
-	private static BigInteger hashint; 
-	
-	public static BigInteger hashOf(String entity) throws NoSuchAlgorithmException {		
-		
+public class Hash {
+
+	private static BigInteger hashint;
+
+	public static BigInteger hashOf(String entity) throws NoSuchAlgorithmException {
+
 		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		MessageDigest md=MessageDigest.getInstance("MD5");
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		
 		// we use MD5 with 128 bits digest
-		
+
 		// compute the hash of the input 'entity'
-		
-		byte[] digest=md.digest(entity.getBytes());
+
+		byte[] digest = md.digest(entity.getBytes());
 		// convert the hash into hex format
-		
-		
+
 		// convert the hex into BigInteger
-	String hex=	toHex(digest);
+		String hex = toHex(digest);
 		// return the BigInteger
-		BigInteger hashhint= new BigInteger(hex,16);
+		BigInteger hashint = new BigInteger(hex, 16);
+		
 		return hashint;
 	}
-	
-	public static BigInteger addressSize() throws NoSuchAlgorithmException  {
-		
+
+	public static BigInteger addressSize() throws NoSuchAlgorithmException {
+
 		// Task: compute the address size of MD5
-		
+
 		// get the digest length
-		// compute the number of bits = digest length * 8(*8 siden digest length returner i bytes-vilde)
-		int nrBits=bitSize();
+		// compute the number of bits = digest length * 8(*8 siden digest length
+		// returner i bytes-vilde)
+		int nrBits = bitSize();
 		// compute the address size = 2 ^ number of bits
-		int Str= (int) Math.pow(2, nrBits);
+		int Str = (int) Math.pow(2, nrBits);
 		// return the address size
-		BigInteger addressSize= BigInteger.valueOf(Str);
+		BigInteger addressSize = BigInteger.valueOf(Str);
 		return addressSize;
 	}
-	
+
 	public static int bitSize() throws NoSuchAlgorithmException {
-		
+
 		int digestlen = 0;
-		
-		digestlen= MessageDigest.getInstance("MD5").getDigestLength();
-		
-		return digestlen*8;
+
+		digestlen = MessageDigest.getInstance("MD5").getDigestLength();
+
+		return digestlen * 8;
 	}
-	
+
 	public static String toHex(byte[] digest) {
 		StringBuilder strbuilder = new StringBuilder();
-		for(byte b : digest) {
-			strbuilder.append(String.format("%02x", b&0xff));
+		for (byte b : digest) {
+			strbuilder.append(String.format("%02x", b & 0xff));
 		}
 		return strbuilder.toString();
 	}
